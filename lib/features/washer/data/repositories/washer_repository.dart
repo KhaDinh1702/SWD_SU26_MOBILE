2e2b7fbcda15017d4d7e2bf50398de5865bc991a
nmChaau <chaunmse180582@fpt.edu.vn>
2026-06-11 11:06:40 +0700

    feat(washer): add washer order model and data layer

---
+++ lib/features/washer/data/repositories/washer_repository.dart
@@ -0,0 +1,29 @@
+import 'package:wave/features/washer/data/models/washer_order_model.dart';
+import 'package:wave/features/washer/data/repositories/washer_api.dart';
+
+class WasherRepository {
+  final WasherApi _api;
+
+  WasherRepository(this._api);
+
+  Future<List<WasherOrderModel>> getWorkOrders() {
+    return _api.getWorkOrders();
+  }
+
+  Future<WasherOrderModel> getWorkOrder(String id) {
+    return _api.getWorkOrder(id);
+  }
+
+  Future<WasherOrderModel> startWorkOrder(String id) {
+    return _api.startWorkOrder(id);
+  }
+
+  Future<WasherOrderModel> updateChecklistItem(
+      String id, int index, bool done) {
+    return _api.updateChecklistItem(id, index, done);
+  }
+
+  Future<WasherOrderModel> finishWorkOrder(String id) {
+    return _api.finishWorkOrder(id);
+  }
+}
